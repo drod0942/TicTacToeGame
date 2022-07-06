b32bb69455cecf3c8834fbe70ddf998bd9d52e68
Darling Rodriguez <darlingrodriguez@Darlings-MacBook-Pro.local>
2022-07-06 15:01:04 -0400

    added a winner function which makes the game functional.

--- javascript.js
+++ javascript.js
@@ -25,13 +25,94 @@ const statusD = document.querySelector('.status');
 const resetD = document.querySelector('.reset');
 const cellDivs = document.querySelectorAll('.grid-cell');
 let gameStart = true;
-let xIsNext = true;
+let xNext = true;
+
+
+//Functions to Update the Game And Check for Winner
+function winner(letter){
+
+    gameStart = false;
+    if (letter === 'x') {
+      statusD.innerHTML = `X has won!`;
+    } else {
+      statusD.innerHTML = `O has won!`;
+    }
+
+  }
+
+
+function checkGame(){
+    //We Want index 2 because we want to know if it is an x, an o, or nothing
+    const tLeft = cellDivs[0].classList[1];
+    const tMiddle = cellDivs[1].classList[1];
+    const tRight = cellDivs[2].classList[1];
+    const mLeft = cellDivs[3].classList[1];
+    const mMiddle = cellDivs[4].classList[1];
+    const mRight = cellDivs[5].classList[1];
+    const bLeft = cellDivs[6].classList[1];
+    const bMiddle = cellDivs[7].classList[1];
+    const bRight = cellDivs[8].classList[1];
+
+
+  // check every way they can win, first we check if 
+  //they exist and then if they are equal to other combos
+
+  if (tLeft && tLeft === tMiddle && tLeft === tRight) { 
+        winner(tLeft);
+  } else if (mLeft && mLeft === mMiddle && mLeft === mRight) {
+        winner(mLeft);
+  } else if (bLeft && bLeft === bMiddle && bLeft === bRight) {
+        winner(bLeft);
+  } else if (tLeft && tLeft === mLeft && tLeft === bLeft) {
+        winner(tLeft);
+  } else if (tMiddle && tMiddle === mMiddle && tMiddle === bMiddle) {
+        winner(tMiddle);
+  } else if (tRight && tRight === mRight && tRight === bRight) {
+        winner(tRight);
+  } else if (tLeft && tLeft === mMiddle && tLeft === bRight) {
+        winner(tLeft);
+  } else if (tRight && tRight === mMiddle && tRight === bLeft) {
+        winner(tRight);
+  } else if (tLeft && tMiddle && tRight && mLeft && mMiddle && mRight && bLeft && bMiddle && bRight) {
+        gameStart = false;
+        statusD.innerHTML = 'Game is tied!';
+  } else {
+
+        xNext = !xNext;
+
+        if (xNext) {
+            statusD.innerHTML = 'X is next';
+        } else {
+            statusD.innerHTML = 'O is next';
+        }
+
+    }
+
+}
+
+
+//event for when reset is clicked
+function handleR(){
+
+    xNext = true;
+    statusD.innerHTML = 'X is next';
+
+    for (const cells of cellDivs) {
+      cells.classList.remove('x');
+      cells.classList.remove('o');
+      cells.classList.remove('won');
+    }
+    gameStart = true;
+
+}
 
 
 //event for when a cell is clicked
-const handleClick = (e) => {
-    const classList = e.target.classList;
+
+function handleClick(evnt){
     
+    const classList = evnt.target.classList;
+
     //Makes sure the same box isnt clicked so it doesnt add two classes
     if (!gameStart || classList[1] === 'x' || classList[1] === 'o') {
         return;
@@ -39,10 +120,13 @@ const handleClick = (e) => {
 
     //makes sure to add a class of x or O when a box is clicked
 
-    if (xIsNext) {
+    if (xNext) {
         classList.add('x');
+        checkGame();
+
       } else {
         classList.add('o');
+        checkGame();
       }
 
   };
